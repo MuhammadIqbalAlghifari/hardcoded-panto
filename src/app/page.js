9a59c9900485a68f94e676649afab571a4f4fa8d
MuhammadIqbalAlghifari <iqbalalghifari18283@gmail.com>
2024-07-16 15:19:39 +0700

    smooth scrolling added

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,6 @@
+"use client"
+
+import { useEffect } from "react";
 import BestSellingProductComponent from "./components/bestProducts";
 import ExperienceComponent from "./components/experiences";
 import FooterComponent from "./components/footer";
@@ -6,8 +9,23 @@ import MaterialsComponent from "./components/materials";
 import NavbarComponent from "./components/navbar";
 import SloganComponent from "./components/slogan";
 import TestimonialsComponent from "./components/testimonials";
+import Lenis from 'lenis'
 
 export default function Home() {
+
+  useEffect(() => {
+    
+    const lenis = new Lenis()
+
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+    
+  }, []);
+
   return (
     <>
       <NavbarComponent/>
